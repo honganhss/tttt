b653789a0b6dc4f745150d816c2c20504cb53953
honganhss <honganh068204@gmai.com>
2024-03-21 09:15:58 +0700

    f

---
+++ WebApplication1/WebApplication1/Bai09/XuLy2.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1.Bai09
+{
+    public partial class XuLy2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            String xml = "<xml>" + 
+                "<h2>Bài 09</h2>" +
+                "<ten>Nguyễn Hồng Ánh <ten>" +
+                "<br><tenVXL>Tên VXL: " + Request.QueryString["cpuName"] + "</tenVXL>" +
+                "<br><hang>Hãng: " + Request.QueryString["cpuFirm"] + "</hang>" +
+                "<br><NgaySX>Ngày SX: " + FormatDate(Request.QueryString["cpuDate"]) + "</NgaySX>" +
+                "<br><Gia>Giá: " + Request.QueryString["cpuPrice"] + "</Gia></xml>";
+            Response.ClearHeaders();
+            Response.AddHeader("conten-type", "text/xml");
+            Response.Write(xml);
+            Response.End();
+
+        }
+
+        string FormatDate(string date)
+        {
+            string[] dateArr = date.Split('-');
+            return dateArr[2] + '/' + dateArr[1] + '/' + dateArr[0];
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Bai09/XyLy.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+namespace WebApplication1.POST
+{
+    public partial class XyLy : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            String xml =
+                "<h2>Bài 09</h2>" +
+                "<h3>Nguyễn Hồng Ánh </h3>" +
+                "<br>Tên VXL: " + Request.Form["cpuName"] +
+                "<br>Hãng: " + Request.Form["cpuFirm"] +
+                "<br>Ngày SX: " + FormatDate(Request.Form["cpuDate"]) +
+                "<br>Giá: " + Request.Form["cpuPrice"];
+            Response.ClearHeaders();
+            Response.AddHeader("conten-type", "text/xml"); 
+            Response.Write(xml);
+            Response.End();
+
+        }
+
+        string FormatDate(string date)
+        {
+            string[] dateArr = date.Split('-');
+            return dateArr[2] + '/' + dateArr[1] + '/' + dateArr[0];
+        }
+    }
+}
